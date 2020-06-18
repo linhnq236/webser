0dc845c071f1e5d0ac8e0b11c8c8f460e913c005
LNQ <linhnq36@gmail.com>
2020-06-18 13:41:54 +0700

    skip auth api

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -1,6 +1,7 @@
 module Api
   class HomeController < ApplicationController
     include ApplicationHelper
+    skip_before_action :authenticate_user!
     skip_before_action :verify_authenticity_token
     FIREBASE_URL    = 'https://iotpro-58c44.firebaseio.com/'
     FIREBASE_SECRET = 'F4mMmNXp1CPYvJYX5KwtrLifqw6UvVO4fyCUKhoj'
