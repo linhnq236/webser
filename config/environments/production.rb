a250b823b98e0dec8c89595651c8749fbe018400
LNQ <linhnq36@gmail.com>
2020-08-07 01:38:13 +0700

    change package.json

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.url = "wss://linhser.herokuapp.com/cable"
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
